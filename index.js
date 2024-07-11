f5d7c014e35ed70c833516d214202d813c9a0593
Ishmael Gyamfi <isgyamfi25@gmail.com>
2024-07-11 21:48:49 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send("Amalitech Give us the Stipends😡!!!");
+});
+
+app.listen(3000, () => {
+    console.log("Server listening on port 3000!!! the stipends ooo");
+});
