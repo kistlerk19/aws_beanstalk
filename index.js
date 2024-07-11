95d8103b51c9fdccd7608d5045870c9e31bc0a6b
Ishmael Gyamfi <isgyamfi25@gmail.com>
2024-07-11 23:18:17 +0000

    port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.status(200).send("Amalitech Give us the Stipends😡!!!");
 });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
     console.log("Server listening on port 3000!!! the stipends ooo");
 });
